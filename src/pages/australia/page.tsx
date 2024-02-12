c7513987fdef4600a026fefa16cad268c9cb001c
bowerska <katelyn.bowers@lcms.org>
2024-02-12 14:26:20 -0600

    Fixed package.json file, changed tab name, and changed link icon

--- src/pages/australia/page.tsx
+++ src/pages/australia/page.tsx
@@ -4,18 +4,15 @@ import { Weather } from '../../components/weather';
 import CountryBigFacts from '../../components/country-big-facts';
 
 export default function Page() {
-	const date = useMemo(() => new Date(), []);
-	const sydneyDate = useMemo(
-		() => date.toLocaleString(undefined, { timeZone: 'Australia/Sydney', timeStyle: 'short', dateStyle: 'short' }),
-		[date]
-	);
+  const date = useMemo(() => new Date(), []);
+  const sydneyDate = useMemo(() => date.toLocaleString(undefined, { timeZone: 'Australia/Sydney', timeStyle: 'short', dateStyle: 'short' }), [date]);
 
-	return (
-		<AppLayout title='Australia'>
-			{/* <CountryBigFacts country='Australia' /> */}
-			<Weather lat={-33.865143} long={151.2099} date={sydneyDate} />
-			<div>Australia TIME</div>
-			<div>Coming Soon...</div>
-		</AppLayout>
-	);
+  return (
+    <AppLayout title="Australia">
+      <CountryBigFacts country="Australia" />
+      <Weather lat={-33.865143} long={151.2099} date={sydneyDate} />
+      <div>Australia TIME</div>
+      <div>Coming Soon...</div>
+    </AppLayout>
+  );
 }
