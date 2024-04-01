5d4634c1aae000960a141bfcabfa0b5f6e454636
bowerska <katelyn.bowers@lcms.org>
2024-04-01 09:30:48 -0500

    Added puppies to home page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,6 +1,23 @@
+import { useCallback, useMemo, useState } from 'react';
 import { AppLayout } from '../layout';
+import { pictures } from './pictures';
+import ImageModal from '../../components/image-modal';
 
 export default function Page() {
+	const [selectedImage, setSelectedImage] = useState<number | null>(null);
+	const [showImageModal, setShowImageModal] = useState(false);
+	const thumbnailLink = useMemo(() => 'https://drive.google.com/thumbnail?id=', []);
+
+	const displayImage = useCallback((img: number) => {
+		setShowImageModal(true);
+		setSelectedImage(img);
+	}, []);
+
+	const close = useCallback(() => {
+		setShowImageModal(false);
+		setSelectedImage(null);
+	}, []);
+
 	return (
 		<AppLayout title='Home'>
 			<h3>Kbow Travels</h3>
@@ -8,6 +25,88 @@ export default function Page() {
 				Pages for my travels in Washington, New Zealand, Australia, etc and Cool Stuff to show some animations I made and some games
 			</p>
 			<img src='https://drive.google.com/thumbnail?id=1S1ClaQgrYTBS7gbR8hRjJTCbW4Ov9ffr' alt='Test 2' className='m-3' />
+
+			<p>Also, here's some cute puppies</p>
+
+			{pictures.map((img) => {
+				if (img.description.startsWith('Dogs')) {
+					return (
+						<span onClick={() => displayImage(pictures.indexOf(img))} key={img.img_id}>
+							<img
+								src={`${thumbnailLink}${img.img_id}`}
+								alt='puppy Img'
+								className='m-3 rounded image-thumbnail'
+								key={img.img_id}
+							/>
+						</span>
+					);
+				} else {
+					return <span key={img.img_id}></span>;
+				}
+			})}
+
+			<br />
+			<br />
+
+			<h3 className='mt-2 ms-3'>Merlinie</h3>
+			{pictures.map((img) => {
+				if (img.description.startsWith('Merlinie')) {
+					return (
+						<span onClick={() => displayImage(pictures.indexOf(img))} key={img.img_id}>
+							<img
+								src={`${thumbnailLink}${img.img_id}`}
+								alt='Merlinie Img'
+								className='m-3 rounded image-thumbnail'
+								key={img.img_id}
+							/>
+						</span>
+					);
+				} else {
+					return <span key={img.img_id}></span>;
+				}
+			})}
+
+			<br />
+			<br />
+			<h3 className='mt-2 ms-3'>Cujo</h3>
+			{pictures.map((img) => {
+				if (img.description.startsWith('Cujo')) {
+					return (
+						<span onClick={() => displayImage(pictures.indexOf(img))} key={img.img_id}>
+							<img
+								src={`${thumbnailLink}${img.img_id}`}
+								alt='Cujo Img'
+								className='m-3 rounded image-thumbnail'
+								key={img.img_id}
+							/>
+						</span>
+					);
+				} else {
+					return <span key={img.img_id}></span>;
+				}
+			})}
+
+			<br />
+			<br />
+			<h3 className='mt-2 ms-3'>Mattie</h3>
+			{pictures.map((img) => {
+				if (img.description.startsWith('Mattie')) {
+					return (
+						<span onClick={() => displayImage(pictures.indexOf(img))} key={img.img_id}>
+							<img
+								src={`${thumbnailLink}${img.img_id}`}
+								alt='Mattie Img'
+								className='m-3 rounded image-thumbnail'
+								key={img.img_id}
+							/>
+						</span>
+					);
+				} else {
+					return <span key={img.img_id}></span>;
+				}
+			})}
+
+			<ImageModal close={close} show={showImageModal} imgIndex={selectedImage} imageArray={pictures} />
 		</AppLayout>
 	);
 }
