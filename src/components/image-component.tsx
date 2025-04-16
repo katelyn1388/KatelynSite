7a0dcbd4b92248c2e1275471466d0a20f6d9fddc
bowerska <katelyn.bowers@lcms.org>
2025-04-16 12:46:34 +1000

    Images added to China page

--- src/components/image-component.tsx
+++ src/components/image-component.tsx
@@ -1,8 +1,25 @@
 import { Img } from 'react-image';
 import ImageUnloaded from './loaders/image-unloaded';
 import DogLoadingAnimation from './loaders/dog-loader';
+import { useEffect, useRef } from 'react';
 
 export function ImageComponent({ imgId, linkEnd }: { imgId: string; linkEnd: string }) {
+	const retryCount = useRef<number>();
+
+	useEffect(() => {
+		if (retryCount.current === undefined) {
+			retryCount.current = 0;
+		}
+	});
+
+	const handleError = () => {
+		console.log('Image failed to load, retrying...');
+		if (retryCount.current !== undefined && retryCount.current < 3) {
+			retryCount.current += 1;
+			window.location.reload();
+		}
+	};
+
 	return (
 		<Img
 			src={`https://lh3.googleusercontent.com/d/${imgId}${linkEnd}`}
@@ -11,6 +28,7 @@ export function ImageComponent({ imgId, linkEnd }: { imgId: string; linkEnd: str
 			className='align-self-center image-thumbnail'
 			unloader={<ImageUnloaded />}
 			key={imgId}
+			onError={handleError}
 		/>
 	);
 }
